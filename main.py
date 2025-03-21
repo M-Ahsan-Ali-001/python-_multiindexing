9898636ce6fbd6f1261d568a9930291593e7a29f
Muhammad Ahsan Ali <91987110+M-Ahsan-Ali-001@users.noreply.github.com>
2025-03-21 19:56:40 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import pdb
+import pandas as pd
+import re 
+
+df = pd.read_excel(r"C:\Users\aa103\OneDrive\Desktop\Book1.xlsx")
+df_cols = df.columns
+test=[]
+for x in range(len(df_cols)-1):
+
+        if re.search('unnamed',df_cols[x],re.IGNORECASE)==None and  re.search('unnamed',df_cols[x+1],re.IGNORECASE)!=None:
+            test.append((df_cols[x],df[df_cols[x]][0]))
+            for y in range(x+1,len(df_cols)):
+                if re.search('unnamed',df_cols[y],re.IGNORECASE)!=None:
+                     test.append((df_cols[x],df[df_cols[y]][0]))
+                else:
+                    break
+
+        
+        elif re.search('unnamed',df_cols[x],re.IGNORECASE)==None and   re.search('propsed',df_cols[x],re.IGNORECASE)!=None :
+                test.append((df_cols[x],df[df_cols[x]][0]))
+        elif   re.search('unnamed',df_cols[x],re.IGNORECASE)==None:
+                test.append((df_cols[x],''))
+
+columns = pd.MultiIndex.from_tuples(test)
+df.columns = columns
+
+
+pdb.set_trace()
